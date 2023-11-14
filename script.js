6d70904325a5a8102bdd4e73f30546074623d7b8
robie081 <robert.lantus@icloud.com>
2023-11-14 10:18:04 +0200

    Casa Lupu

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
-// theWhippet Like Hamburger with SVG and Second
 
-// const menuButton = document.querySelector('.menu-button');
-const menuIcon = document.querySelector('.menu-icon');
-const hamburger = document.querySelector('.hamburger');
-const cross = document.querySelector('.cross');
+// Casa Lupu
 
-menuIcon.addEventListener('click', () => {
+const navSlide = () => {
 
-    if (hamburger.style.display === 'none') {
-        hamburger.style.display = 'block';
-        cross.style.display = 'none';
-        // menuButton.blur();
-    } else {
-        hamburger.style.display = 'none';
-        cross.style.display = 'block';
-        // menuButton.blur();
+    const hamburger = document.querySelector('.hamburger');
+    const cross = document.querySelector('.cross');
+    const nav = document.querySelector('.nav-links');
+    // const navLinks = document.querySelectorAll('.nav-links li');
+
+    const main = document.querySelector('main');
+
+    // Hamburger Animation and Nav Toggle
+
+    const toggler = () => {
+        hamburger.classList.toggle('changeNone');
+        // cross.classList.toggle('changeBlock');
+        cross.classList.toggle('visible');
+        nav.classList.toggle('nav-active');
+        
     }
-});
+
+    hamburger.addEventListener('click', () => {
+        toggler();
+        main.style.opacity = 0.1;
+    });
+
+    cross.addEventListener('click', () => {
+        toggler();
+        main.style.opacity = 1;
+    });
+}
+
+navSlide();
+
+
