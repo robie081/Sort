16d326c41b685b51946929ae13ea80339b6b9686
robie081 <robert.lantus@icloud.com>
2023-11-14 16:08:01 +0200

    Terox Travel

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
 
-// Casa Lupu
+// Retrieved from robertlantus/menus/
+
 
 const navSlide = () => {
-
     const hamburger = document.querySelector('.hamburger');
-    const cross = document.querySelector('.cross');
     const nav = document.querySelector('.nav-links');
-    // const navLinks = document.querySelectorAll('.nav-links li');
-
+    const navLinks = document.querySelectorAll('.nav-links li');
     const main = document.querySelector('main');
 
     // Hamburger Animation and Nav Toggle
 
-    const toggler = () => {
-        hamburger.classList.toggle('changeNone');
-        // cross.classList.toggle('changeBlock');
-        cross.classList.toggle('visible');
-        nav.classList.toggle('nav-active');
-        
-    }
-
     hamburger.addEventListener('click', () => {
-        toggler();
-        main.style.opacity = 0.1;
-    });
+        hamburger.classList.toggle('change');
+        nav.classList.toggle('nav-active');
+        main.classList.toggle('opacity');
 
-    cross.addEventListener('click', () => {
-        toggler();
-        main.style.opacity = 1;
+        // Animate Links
+        navLinks.forEach((link, index) => {
+        // console.log(index, index / 7);
+        // console.log(link.style.animation);
+        if (link.style.animation) 
+            link.style.animation = '';
+        else
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+        });
     });
 }
 
 navSlide();
 
+
+
+
+// TEROX Travel Hamburger
+
+// const hamburgerMenu = () => {
+//     const hamburger = document.querySelector('.hamburger');
+
+//     hamburger.addEventListener('click', () => {
+//         hamburger.classList.toggle('change');
+//     });
+// }
+
+// hamburgerMenu();
 
